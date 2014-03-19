b4c6abf429ef2d72d07bc513cd41aa7435f4a850
AllenDang <allengnr@gmail.com>
2014-03-19 18:25:42 +0800

    添加AndroidBarSherlock引用

---
+++ src/com/shanpow/app/MainActivity.java
@@ -0,0 +1,9 @@
+package com.shanpow.app;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.main)
+public class MainActivity extends SherlockActivity {
+
+}
