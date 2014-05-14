3f22a40b1b888de220e50fc82f41f1f1571aea47
AllenDang <allengnr@gmail.com>
2014-05-14 12:15:32 +0800

    删除调试log

--- android/src/main/java/com/shanpow/app/util/WebkitCookieManagerProxy.java
+++ android/src/main/java/com/shanpow/app/util/WebkitCookieManagerProxy.java
@@ -1,7 +1,5 @@
 package com.shanpow.app.util;
 
-import android.util.Log;
-
 import java.io.IOException;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
@@ -43,7 +41,6 @@ public class WebkitCookieManagerProxy extends CookieManager {
 
             // process each of the headers
             for (String headerValue : responseHeaders.get(headerKey)) {
-                Log.d("DEBUG", headerValue);
                 this.webkitCookieManager.setCookie(url, headerValue);
             }
         }
