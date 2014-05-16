672b9a4f1bbdd1240110aa65567b4bb1c898cc4b
AllenDang <allengnr@gmail.com>
2014-05-16 10:35:06 +0800

    添加ActivityLauncher，用于让javascript可以启动Activity

---
+++ android/src/main/java/com/shanpow/app/cordovaplugin/ActivityLauncher.java
@@ -0,0 +1,34 @@
+package com.shanpow.app.cordovaplugin;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.shanpow.app.android.LoginActivity_;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * Created by allendang on 14-5-16.
+ */
+public class ActivityLauncher extends CordovaPlugin {
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("login")) {
+            login();
+        } else {
+            return false;
+        }
+
+        callbackContext.success();
+        return true;
+    }
+
+    public void login() {
+        Context context = cordova.getActivity().getApplicationContext();
+        Intent intent = new Intent(context, LoginActivity_.class);
+        context.startActivity(intent);
+    }
+}
