8258f504a7670d0656ff47ae8248e02bffad7762
AllenDang <allengnr@gmail.com>
2014-06-04 18:18:16 +0800

    文章来源列表界面初步完成

---
+++ android/src/main/java/com/shanpow/app/entity/BookSource.java
@@ -0,0 +1,11 @@
+package com.shanpow.app.entity;
+
+/**
+ * Created by allendang on 14-6-4.
+ */
+public class BookSource {
+    public String Host;
+    public String ChapterUrl;
+    public String Chapter;
+    public String UpdateTime;
+}
---
+++ android/src/main/java/com/shanpow/app/entity/GetBookSourcesResult.java
@@ -0,0 +1,8 @@
+package com.shanpow.app.entity;
+
+/**
+ * Created by allendang on 14-6-4.
+ */
+public class GetBookSourcesResult extends AjaxResultBase {
+    public BookSource[] Data;
+}
