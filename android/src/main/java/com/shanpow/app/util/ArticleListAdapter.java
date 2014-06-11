53a7014e739bb81cfb9ee4120995b7d99f63396d
AllenDang <allengnr@gmail.com>
2014-06-11 18:49:24 +0800

    文章列表布局调整，并加上图片

--- android/src/main/java/com/shanpow/app/util/ArticleListAdapter.java
+++ android/src/main/java/com/shanpow/app/util/ArticleListAdapter.java
@@ -5,10 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.shanpow.app.android.R;
 import com.shanpow.app.entity.SimpleArticleInfo;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by allendang on 14-6-11.
@@ -28,13 +30,17 @@ public class ArticleListAdapter extends ArrayAdapter<SimpleArticleInfo> {
         TextView txtv_title = (TextView) convertView.findViewById(R.id.txtv_title);
         TextView txtv_author = (TextView) convertView.findViewById(R.id.txtv_author);
         TextView txtv_onlinetime = (TextView) convertView.findViewById(R.id.txtv_onlinetime);
-        //ImageView img_banner = (ImageView) convertView.findViewById(R.id.img_banner);
+        ImageView img_banner = (ImageView) convertView.findViewById(R.id.img_banner);
 
         SimpleArticleInfo info = getItem(position);
 
         txtv_title.setText(info.Title);
         txtv_author.setText(info.Author);
-        txtv_onlinetime.setText(info.OnlineTime);
+        txtv_onlinetime.setText(String.format("%s前", info.OnlineTime));
+
+        Picasso.with(getContext())
+                .load(info.ImageUrl + "?imageView/2/h/68")
+                .into(img_banner);
 
         return convertView;
     }
